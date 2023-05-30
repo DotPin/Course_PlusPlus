8cc81106fa5fc64d13a3cb305db4e8145ce4bc98
dotpin <diego.rojas.asenjo@alumnos.uach.cl>
2023-05-29 22:13:16 -0400

    Métodos Estáticos

---
+++ Modulo 1 - Elemental/Seccion 53 - Metodos estáticos de una clase/problema1/archivo1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+class Prueba {
+        static int x1;
+    public:
+        static int retornarX();
+    };
+
+int Prueba::x1 = 20;
+
+int Prueba::retornarX(){
+    return x1;
+}
+
+int main(){
+    cout << Prueba::retornarX(); // 20
+    return 0;
+}
